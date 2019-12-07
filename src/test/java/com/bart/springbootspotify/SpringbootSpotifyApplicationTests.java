19d4500884ab9a34793c8cc88a1b95c250f4e826
bart7801 <bart7801@o2.pl>
2019-12-07 00:37:29 +0000

    Initial commit

---
+++ src/test/java/com/bart/springbootspotify/SpringbootSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bart.springbootspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
